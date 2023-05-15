7cd6e0382cf048b967c81f1447b1bc350d08b5b2
CriticalDesign <aaron.langille@cambriancollege.ca>
2023-05-15 15:04:54 -0400

    Removed some unused Using statements.

--- StarryNightExample/Game1.cs
+++ StarryNightExample/Game1.cs
@@ -3,7 +3,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System.Collections.Generic;
 using System;
-using Microsoft.Xna.Framework.Media;
+
 
 namespace StarryNightExample
 {
